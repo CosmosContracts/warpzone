e76c9df51e3bdb7632a68724351396de7c98fae5
vexxvakan <hiimvexx@gmail.com>
2022-02-08 10:59:32 +0100

    refactor(General): fine-tuned three js animation sequence

--- components/sections/Background/Camera.tsx
+++ components/sections/Background/Camera.tsx
@@ -45,7 +45,7 @@ export const Camera = ({ ...props }) => {
 		void cameraControlsRef.current?.setLookAt(0, 0, 900, 0, 100, 0, true)
 		void cameraControlsRef.current?.rotateAzimuthTo(1, true)
 		void cameraControlsRef.current?.rotatePolarTo(2.35, true)
-		void cameraControlsRef.current?.truck(630, -100, true)
+		void cameraControlsRef.current?.truck(730, 0, true)
 	}
 
 	useEffect(() => {
